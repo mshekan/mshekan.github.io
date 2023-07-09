5319c705896d5e2b01b2ec33209fc0811670b474
Your Name <you@example.com>
2023-07-10 01:18:45 +0330

    MOHANDES MOUSAVI

--- js/tarikh.js
+++ js/tarikh.js
@@ -3,16 +3,22 @@ var chandom = date.getDate();
 var borj = date.getMonth();
 var hafte = date.getDay();
 var shanbe = [" یکشنبه "," دوشنبه ", " سه‌شنبه ", " چهارشنبه ", " پنجشنبه ", " جمعه ", " شنبه "];
-var maaah = ["0", "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهــر", "آبان", "آذر", "دی", "بهمـن", "اسفند",];
+var maah = ["0", "فـروردین", "اردیبهشت", "خـــرداد", "تیـــر", "مـــرداد", "شهــریـور", "مـهــــر", "آبــان", "آذر", "دی", "بهمـــن", "اسـفــند"];
 var xRoooz;
-if(chandom >= 21){
-xRoooz = chandom -= 21;
-borj -= 3;
+var xborj;
+if(chandom >= 20){
+xRoooz = chandom -= 20;
+xborj = borj -= 3;
 }else{
-xRoooz = chandom += 10;
-borj -= 2;
+xRoooz = chandom += 9;
+xborj = borj -= 2;
 }
+var myborj;
+if(xborj = 4){
+myborj = "تیـــر"
+}
+
 document.getElementById("xday").innerHTML =xRoooz + "/";
 document.getElementById("xhafte").innerHTML = shanbe[hafte];
-document.getElementById("xmah").innerHTML = borj;
-document.getElementById("xmaah").innerHTML = "/" + maaah[borj];
+
+
