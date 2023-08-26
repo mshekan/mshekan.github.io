01c4b8144a91f1e3b6a1b6b515ec1c503a61439a
Your Name <you@example.com>
2023-08-26 04:04:28 +0330

    MOHANDES MOUSAVI

--- js/tarikh.js
+++ js/tarikh.js
@@ -1,18 +1,18 @@
 var date = new Date();
 var chandom = date.getDate();
-var borj = date.getMonth();
+var mh = date.getMonth();
 var hafte = date.getDay();
 var shanbe = [" یکشنبه "," دوشنبه ", " سه‌شنبه ", " چهارشنبه ", " پنجشنبه ", " جمعه ", " شنبه "];
-var maah = ["0", "فـروردین", "اردیبهشت", "خـــرداد", "تیـــر", "مـــرداد", "شهــریـور", "مـهــــر", "آبــان", "آذر", "دی", "بهمـــن", "اسـفــند"];
+var maah = ["فـروردین", "اردیبهشت", "خـــرداد", "تیـــر", "مـــرداد", "شهــریـور", "مـهــــر", "آبــان", "آذر", "دی", "بهمـــن", "اسـفــند"];
 var xmiladi = ["1", "2", "3", "4", "21", "21", "20", "20", "20", "", "", ""]
 var xRoooz;
 var xborj;
 if(chandom >= 21){
 xRoooz = chandom -= 22;
-xborj = borj -= 3;
+xborj = mh -= 2;
 }else{
 xRoooz = chandom += 9;
-xborj = borj -= 2;
+xborj = mh -= 3;
 }
 
 
